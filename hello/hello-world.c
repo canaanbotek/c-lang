653647df869afab95f44e8d9d7da2ff7e9ae8646
Christian Canaan <botek.b1991@gmail.com>
2024-06-21 18:45:29 -0300

    primer prueba

---
+++ hello/hello-world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+/* Aquí va el cuerpo del programa */
+printf("Hola mundo\n");
+return EXIT_SUCCESS;
+}
+
+/* Para la ejecución:
+1) Estar en el directorio donde está nuestro archivo .c
+2) Debemos compilarlo, para eso, podemos usar el comando gcc
+gcc hello-world.c -o hello-world
+3) El paso 2 nos creara otro archivo de nombre hello-world que será nuestro codigo compilado
+4) Para ejecutarlo simplemente escribimos en el terminal
+./hello-world
+5) Vemos los resultados
+*/
